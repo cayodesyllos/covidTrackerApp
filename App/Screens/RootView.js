b4082a1a306fca85cdd77a377a6217d34019f447
Cayo Syllos <cayodesyllos@gmail.com>
2021-04-29 11:44:02 -0300

    notifications screen

--- App/Screens/RootView.js
+++ App/Screens/RootView.js
@@ -213,9 +213,7 @@ const Locations = (props) => {
   const [search, setSearch] = React.useState([]);
 
   useEffect(() => {
-    if (!props.checkedIn) {
-      mountRequests();
-    }
+    mountRequests();
   }, [props.checkedIn]);
 
   useEffect(() => {}, [checkins]);
@@ -241,7 +239,7 @@ const Locations = (props) => {
       }}>
       <TouchableWithoutFeedback
         onPress={() => {
-          navigationService.LocationDetail(props.componentId, item);
+          // navigationService.LocationDetail(props.componentId, item);
         }}
         style={{flexDirection: 'row', justifyContent: 'space-between'}}>
         <FontAwesomeIcon
@@ -426,7 +424,86 @@ const Locations = (props) => {
 };
 
 const Notifications = (props) => {
-  return <ScrollView contentContainerStyle={MainStyles.container} />;
+  const [notifications, setNotifications] = React.useState([]);
+
+  useEffect(() => {
+    mountRequests();
+  }, []);
+
+  const mountRequests = async () => {
+    try {
+      const response = await api.get('/notifications');
+      setNotifications(response.data);
+    } catch (error) {
+      console.warn(error.message);
+    }
+  };
+
+  const renderItem = ({item}) => (
+    <View
+      style={{
+        width: '80%',
+        height: 90,
+        alignSelf: 'center',
+        borderBottomColor: 'white',
+        padding: 10,
+        borderRadius: 10,
+        borderWidth: 0,
+        backgroundColor: Colors.DARK_GRAY,
+        marginBottom: 10,
+      }}>
+      <TouchableWithoutFeedback
+        onPress={() => {
+          // navigationService.LocationDetail(props.componentId, item);
+        }}
+        style={{flexDirection: 'column', justifyContent: 'space-evenly'}}>
+        <View
+          style={{
+            flexDirection: 'row',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+          }}>
+          <FontAwesomeIcon
+            style={{
+              color: item.flag,
+              fontSize: 40,
+              alignSelf: 'center',
+            }}
+            size={40}
+            icon={faBell}
+          />
+          <Text
+            style={[
+              MainStyles.regularText,
+              {fontSize: 14, width: '80%', padding: 3},
+            ]}>
+            Someone infected crossed paths with you at {item.name} at{' '}
+            {new Date(item.created_at).toLocaleString()}
+          </Text>
+        </View>
+        <Text style={[MainStyles.regularTextMini, {alignSelf: 'flex-end'}]}>
+          {new Date(item.updated_at).toLocaleString()}
+        </Text>
+      </TouchableWithoutFeedback>
+    </View>
+  );
+
+  return (
+    <ScrollView contentContainerStyle={MainStyles.container}>
+      <View style={{height: '80%', width: '100%'}}>
+        <FlatList
+          style={{width: '100%'}}
+          contentContainerStyle={{
+            width: '100%',
+            justifyContent: 'center',
+          }}
+          keyExtractor={(item) => item.id}
+          data={notifications}
+          renderItem={renderItem}
+        />
+      </View>
+    </ScrollView>
+  );
 };
 
 const CustomTab = (props) => {
@@ -532,9 +609,9 @@ const RootView = (props) => {
     const parts = event.url.split('//');
     const token = parts[1];
     try {
-      setCheckedIn(true);
       const response = await api.post('/checkin', {token: token});
       alert(`Checked in at ${response.data.location_name}`);
+      setCheckedIn(!checkedIn);
     } catch (error) {
       alert(error.response.data.error.message);
     }
@@ -542,6 +619,8 @@ const RootView = (props) => {
   };
 
   const handleInitialLoad = async () => {
+    const response = await api.get('/user');
+    setHealthStatus(response.data.infected ? 2 : 1);
     setLoading(false);
   };
 
