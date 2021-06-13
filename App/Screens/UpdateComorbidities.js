5bbfe575184ec60edc9980b06f47713611cdec00
Cayo Syllos <cayodesyllos@gmail.com>
2021-06-13 18:04:16 -0300

    update comorbidities and vacine

--- App/Screens/UpdateComorbidities.js
+++ App/Screens/UpdateComorbidities.js
@@ -1,12 +1,41 @@
 import React, {Component} from 'react';
-import {View} from 'react-native';
-import {StyleSheet, TouchableHighlight, Text} from 'react-native';
+import {
+  View,
+  StyleSheet,
+  TouchableHighlight,
+  Text,
+  TouchableOpacity,
+} from 'react-native';
 import navigationService from '../services/NavigationService';
 import MainStyles from '../style/MainStyles';
+import Colors from '../style/Colors';
 import api from '../services/api';
-import AsyncStorage from '@react-native-community/async-storage';
 
 export default class UpdateComorbidities extends Component {
+  state = {
+    cardiovascular: false,
+    diabetis: false,
+    lung: false,
+    cancer: false,
+    kidney: false,
+    solid_organ: false,
+    obesity: false,
+    smoking: false,
+  };
+
+  componentDidMount() {
+    this.initialLoad();
+  }
+
+  initialLoad = async () => {
+    const comorbidities = await api.get('/comorbity');
+    this.setState(comorbidities.data);
+  };
+
+  update = async () => {
+    await api.put('/comorbity', this.state);
+  };
+
   render() {
     return (
       <View
@@ -15,11 +44,187 @@ export default class UpdateComorbidities extends Component {
           justifyContent: 'space-evenly',
           flex: 1,
         }}>
-        <View style={{height: 100}}>
+        <View
+          style={{
+            marginVertical: 20,
+          }}>
           <Text style={MainStyles.TextTitle}>Update Comorbidities</Text>
         </View>
 
-        <View style={{height: 300}} />
+        <View style={{alignItems: 'center'}}>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.cardiovascular
+                  ? Colors.PURPLE
+                  : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({cardiovascular: !this.state.cardiovascular});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.cardiovascular ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Cardiovascular desease
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.diabetis ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({diabetis: !this.state.diabetis});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.diabetis ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Diabetis melitus
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.lung ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({lung: !this.state.lung});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.lung ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Lung desease
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.cancer ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({cancer: !this.state.cancer});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.cancer ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Cancer
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.kidney ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({kidney: !this.state.kidney});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.kidney ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Kidney desease
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.solid_organ
+                  ? Colors.PURPLE
+                  : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({solid_organ: !this.state.solid_organ});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.solid_organ ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Solid organ or hematopoietic {'\n'}stem cell transplantation
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.obesity ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({obesity: !this.state.obesity});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.obesity ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Obesity
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[
+              MainStyles.selectContainer,
+              MainStyles.regularButton,
+              {
+                backgroundColor: this.state.smoking ? Colors.PURPLE : 'white',
+              },
+            ]}
+            onPress={async () => {
+              this.setState({smoking: !this.state.smoking});
+            }}>
+            <Text
+              style={[
+                MainStyles.regularText,
+                {
+                  color: !this.state.smoking ? Colors.PURPLE : 'white',
+                },
+              ]}>
+              Smoking
+            </Text>
+          </TouchableOpacity>
+        </View>
         <View
           style={{
             height: 120,
@@ -29,7 +234,12 @@ export default class UpdateComorbidities extends Component {
           <TouchableHighlight
             style={[MainStyles.buttonContainer, MainStyles.regularButton]}
             onPress={async () => {
-              navigationService.RootView();
+              try {
+                await this.update();
+                navigationService.RootView();
+              } catch (error) {
+                alert('Network error');
+              }
             }}>
             <Text style={MainStyles.regularText}>Update</Text>
           </TouchableHighlight>
--- App/Screens/UpdateVaccine.js
+++ App/Screens/UpdateVaccine.js
@@ -1,12 +1,32 @@
 import React, {Component} from 'react';
 import {View} from 'react-native';
-import {StyleSheet, TouchableHighlight, Text} from 'react-native';
+import {StyleSheet, TouchableHighlight, Text, Picker} from 'react-native';
 import navigationService from '../services/NavigationService';
 import MainStyles from '../style/MainStyles';
 import api from '../services/api';
+import {TextInputMask} from 'react-native-masked-text';
 import AsyncStorage from '@react-native-community/async-storage';
 
 export default class UpdateVaccine extends Component {
+  state = {
+    vacine: '',
+    date_first_dose: '',
+    date_second_dose: '',
+  };
+
+  componentDidMount() {
+    this.initialLoad();
+  }
+
+  initialLoad = async () => {
+    const response = await api.get('/vacine');
+    this.setState(response.data);
+  };
+
+  update = async () => {
+    await api.put('/vacine', this.state);
+  };
+
   render() {
     return (
       <View
@@ -19,7 +39,53 @@ export default class UpdateVaccine extends Component {
           <Text style={MainStyles.TextTitle}>Update Vaccine</Text>
         </View>
 
-        <View style={{height: 300}} />
+        <View style={{alignItems: 'center'}}>
+          <View style={MainStyles.dropDownContainer}>
+            <Picker
+              selectedValue={this.state.vacine}
+              style={[{width: '100%'}, MainStyles.inputs]}
+              onValueChange={(itemValue, itemIndex) =>
+                this.setState({vacine: itemValue})
+              }>
+              <Picker.Item label="Choose vaccine..." value="" />
+              <Picker.Item label="Pfizer" value="Pfizer" />
+              <Picker.Item label="Astrazeneca" value="Astrazeneca" />
+              <Picker.Item label="Coronavac" value="Coronavac" />
+            </Picker>
+          </View>
+          <View style={MainStyles.inputContainer}>
+            <TextInputMask
+              style={MainStyles.inputs}
+              placeholder="Date First Dose"
+              type={'datetime'}
+              options={{
+                format: 'DD/MM/YYYY',
+              }}
+              value={this.state.date_first_dose}
+              onChangeText={(date_first_dose) => {
+                this.setState({
+                  date_first_dose,
+                });
+              }}
+            />
+          </View>
+          <View style={MainStyles.inputContainer}>
+            <TextInputMask
+              style={MainStyles.inputs}
+              placeholder="Date Second Dose"
+              type={'datetime'}
+              options={{
+                format: 'DD/MM/YYYY',
+              }}
+              value={this.state.date_second_dose}
+              onChangeText={(date_second_dose) => {
+                this.setState({
+                  date_second_dose,
+                });
+              }}
+            />
+          </View>
+        </View>
         <View
           style={{
             height: 120,
@@ -29,7 +95,12 @@ export default class UpdateVaccine extends Component {
           <TouchableHighlight
             style={[MainStyles.buttonContainer, MainStyles.regularButton]}
             onPress={async () => {
-              navigationService.RootView();
+              try {
+                await this.update();
+                navigationService.RootView();
+              } catch (error) {
+                alert('Network error');
+              }
             }}>
             <Text style={MainStyles.regularText}>Update</Text>
           </TouchableHighlight>
